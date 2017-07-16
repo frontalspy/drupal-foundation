b3c2891fdfa6a99cb6a2d5e44501eb5a4b4af38e
Brendon <websupport@rotapix.com>
2017-07-17 09:09:09 +1000

    Initial Push

---
+++ assets/scripts/customizer.js
@@ -0,0 +1,12 @@
+(function($) {
+    // Site title
+    wp.customize(
+        'blogname', function(value) {
+            value.bind(
+                function(to) {
+                    $('.brand').text(to);
+                }
+            );
+        }
+    );
+})(jQuery);
